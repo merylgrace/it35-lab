d46f360f8ecaf6c9d2043a37d42f97aa12bd2e14
emerita <luntian429.21@gmail.com>
2025-02-18 08:16:01 +0800

    Updates

--- assets/swipe-back-legacy-CG0NNdzl.js
+++ assets/swipe-back-legacy-CG0NNdzl.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-aC0uSwYs.js","./index3-legacy-6XZPdus4.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BXFuplxk.js","./index3-legacy-C48aDJbO.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
