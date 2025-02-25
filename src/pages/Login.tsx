bae7f9ff10caac7ac31b519f78362c7159f45a9e
emerita <luntian429.21@gmail.com>
2025-02-25 08:45:14 +0800

    modified the login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,9 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
   IonMenuButton,
   IonPage,
   IonTitle,
@@ -10,11 +13,14 @@ import {
   useIonRouter
 } from '@ionic/react';
 
+import { personCircleOutline } from 'ionicons/icons';
+
 function Login() {
   const navigation = useIonRouter();
   const doLogin = () => {
     navigation.push('/it35-lab/app', 'forward', 'replace');
   };
+
   return (
     <IonPage>
       <IonHeader>
@@ -26,6 +32,15 @@ function Login() {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <IonIcon icon={personCircleOutline} style={{ fontSize: '60px' }} />
+        </div>
+        <IonItem>
+          <IonInput label="Username" placeholder="Enter Username"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput label="Password input" type="password" value="password"></IonInput>
+        </IonItem>
         <IonButton onClick={() => doLogin()} expand="full">
           Login
         </IonButton>
