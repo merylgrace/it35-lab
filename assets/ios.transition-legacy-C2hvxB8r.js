688e15041e17ce03eb4b337da37f3cfc935f6693
emerita <luntian429.21@gmail.com>
2025-03-29 16:06:49 +0800

    Updates

--- assets/ios.transition-legacy-C2hvxB8r.js
+++ assets/ios.transition-legacy-C2hvxB8r.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-iBj5lhZW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D3RBKz-r.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
